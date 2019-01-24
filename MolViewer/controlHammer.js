773b2b38d03e720702b98839d8003d9faa919656
GraceBa <45609466+GraceBa@users.noreply.github.com>
2019-01-25 10:28:32 +1100

    Update controlHammer.js

--- MolViewer/controlHammer.js
+++ MolViewer/controlHammer.js
@@ -1,71 +1,91 @@
 AFRAME.registerComponent("foo",{
     init:function() {
         var element = document.querySelector('body');
-        var newmarker = document.getElementById('markerInput').value;
-        this.marker = document.getElementById(newmarker);
-        var newmodel = document.getElementById('modelInput').value;
-        var model = document.getElementById(newmarker + newmodel);
+        var markername = document.getElementById('markerInput').value;
+        this.marker = document.getElementById(markername);
+        var modelname = document.getElementById('modelInput').value;
+        var model = document.getElementById(markername + modelname);
         this.markerVisible = false
         var hammertime = new Hammer(element);
         var pinch = new Hammer.Pinch();
         hammertime.add(pinch); // add it to the Manager instance
       
         hammertime.on('pan', (ev) => {
-          /*  console.log(ev.scale);
-            var newScale = model.getAttribute("scale");
-            console.log(newScale);
-            document.getElementById('info').innerHTML = ev.scale;*/
-            newmarker = document.getElementById('markerInput').value;
-            this.marker = document.getElementById(newmarker);
-            newmodel = document.getElementById('modelInput').value;
-            model = document.getElementById(newmarker + newmodel);
-            if (!model) { return;}
-            if (!this.markerVisible) { return;}
-            let rotation = model.getAttribute("rotation")
-            switch(ev.direction) {
-                case 2:
-                rotation.y = rotation.y + 4
-                break;
-                case 4:
-                rotation.y = rotation.y - 4
-                break;
-                case 8:
-                rotation.x = rotation.x + 4
-                break;
-                case 16:
-                rotation.x = rotation.x - 4
-                break;
-                default:
-                break;
-            } 
+            markername = document.getElementById('markerInput').value;
+            this.marker = document.getElementById(markername);
+            var i;
+
+            var currentInstances = document.getElementById(markername).getElementsByTagName('a-entity').length
+            if (!document.getElementById('sync').checked) {
+                currentInstances = 1;
+                modelname = document.getElementById('modelInput').value;
+                model = document.getElementById(markername + modelname);
+                if (!model) { return;}
+            }
+            for (i = 0; i < currentInstances; i++) {
+                if (document.getElementById('sync').checked) {
+                    var identity = document.getElementById(markername).getElementsByTagName('a-entity')[i].id;
+                    var model = document.getElementById(identity);
+                }
+                if (!this.markerVisible) { return; }
+                let rotation = model.getAttribute("rotation")
+                switch (ev.direction) {
+                    case 2:
+                        rotation.y = rotation.y + 4
+                        break;
+                    case 4:
+                        rotation.y = rotation.y - 4
+                        break;
+                    case 8:
+                        rotation.x = rotation.x + 4
+                        break;
+                    case 16:
+                        rotation.x = rotation.x - 4
+                        break;
+                    default:
+                        break;
+                }
                 model.setAttribute("rotation", rotation);
+            }
         }); 
       
         hammertime.on("pinch", (ev) => {
-            //document.getElementById('info').innerHTML = ev.scale;
-            newmarker = document.getElementById('markerInput').value;
-            this.marker = document.getElementById(newmarker);
-            newmodel = document.getElementById('modelInput').value;
-            model = document.getElementById(newmarker + newmodel);
-            if (!model) { return; }
-            if (!this.markerVisible) { return; }
-            //this bases the new position off the current
-            var limit = 1;
-            var factor = 0.2
-            let originalScale = model.getAttribute('scale');
-            if ((ev.scale - limit) * factor + originalScale.x <= 0) {
-                ev.scale = limit;
+            markername = document.getElementById('markerInput').value;
+            this.marker = document.getElementById(markername);
+            var i;
+
+            var currentInstances = document.getElementById(markername).getElementsByTagName('a-entity').length
+            if (!document.getElementById('sync').checked) {
+                currentInstances = 1;
+                modelname = document.getElementById('modelInput').value;
+                model = document.getElementById(markername + modelname);
+                if (!model) { return;}
             }
-            if ((ev.scale - limit) * factor + originalScale.x >= 10) {
-                ev.scale = limit;
+            for (i = 0; i < currentInstances; i++) {
+                if (document.getElementById('sync').checked) {
+                    var identity = document.getElementById(markername).getElementsByTagName('a-entity')[i].id;
+                    var model = document.getElementById(identity);
+                }
+
+                if (!this.markerVisible) { return; }
+                //this bases the new position off the current
+                var limit = 1;
+                var factor = 0.2
+                let originalScale = model.getAttribute('scale');
+                if ((ev.scale - limit) * factor + originalScale.x <= 0) {
+                    ev.scale = limit;
+                }
+                if ((ev.scale - limit) * factor + originalScale.x >= 10) {
+                    ev.scale = limit;
+                }
+                let scale = { x: (ev.scale - limit) * factor + originalScale.x, y: (ev.scale - limit) * factor + originalScale.y, z: (ev.scale - limit) * factor + originalScale.z };
+                model.setAttribute("scale", scale);
+                //alternatively, this scales absolutely
+                /*
+                let scale = { x: ev.scale, y: ev.scale, z: ev.scale }
+                model.setAttribute("scale", scale); 
+                */
             }
-            let scale = { x: (ev.scale - limit) * factor + originalScale.x, y: (ev.scale - limit) * factor + originalScale.y, z: (ev.scale - limit) * factor + originalScale.z };
-            model.setAttribute("scale", scale);
-            //alternatively, this scales absolutely
-            /*
-            let scale = { x: ev.scale, y: ev.scale, z: ev.scale }
-            model.setAttribute("scale", scale); 
-            */
         });
 
     },
